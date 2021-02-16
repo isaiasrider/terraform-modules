f3e886a7c66b4d36b41bf2ca7e6213d101f7e046
isaiasrider <isaiasdelima1@gmail.com>
2021-02-16 09:43:47 -0300

    fix resource

--- modules/services/webserver_cluster/schedules.tf
+++ modules/services/webserver_cluster/schedules.tf
@@ -1,5 +1,5 @@
-resource "aws_autoscaling_group" "scale-out-during-business-hours" {
-  schedule_action_name = "${var.cluster_name}-scale-out-during-business-hour"
+resource "aws_autoscaling_schedule" "scale-out-during-business-hours" {
+  scheduled_action_name = "${var.cluster_name}-scale-out-during-business-hour"
   count = var.enable_autoscaling ? 1 : 0
   max_size = 2
   min_size = 10
@@ -8,8 +8,8 @@ resource "aws_autoscaling_group" "scale-out-during-business-hours" {
   autoscaling_group_name = aws_autoscaling_group.asg_web.name
 }
 
-resource "aws_autoscaling_group" "scale-in-at-nigh" {
-  schedule_action_name = "${var.cluster_name}-scale-in-at-night"
+resource "aws_autoscaling_schedule" "scale-in-at-nigh" {
+  scheduled_action_name = "${var.cluster_name}-scale-in-at-night"
   count = var.enable_autoscaling ? 0 : 1
   max_size = 2
   min_size = 10
