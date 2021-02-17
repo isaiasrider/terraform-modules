c47ed9b1ae3cf816b1a4536ebd9e767282a8336d
isaiasrider <isaiasdelima1@gmail.com>
2021-02-16 21:02:44 -0300

    fix min max size of schedule actions

--- modules/services/webserver_cluster/schedules.tf
+++ modules/services/webserver_cluster/schedules.tf
@@ -1,8 +1,8 @@
 resource "aws_autoscaling_schedule" "scale-out-during-business-hours" {
   scheduled_action_name = "${var.cluster_name}-scale-out-during-business-hour"
   count = var.enable_autoscaling ? 1 : 0
-  max_size = 2
-  min_size = 10
+  min_size = 2
+  max_size = 10
   desired_capacity = 10
   recurrence = "0 9 * * *"
   autoscaling_group_name = aws_autoscaling_group.asg_web.name
@@ -11,8 +11,8 @@ resource "aws_autoscaling_schedule" "scale-out-during-business-hours" {
 resource "aws_autoscaling_schedule" "scale-in-at-nigh" {
   scheduled_action_name = "${var.cluster_name}-scale-in-at-night"
   count = var.enable_autoscaling ? 0 : 1
+  min_size = 1
   max_size = 2
-  min_size = 10
   desired_capacity = 2
   recurrence = "0 17 * * *"
   autoscaling_group_name = aws_autoscaling_group.asg_web.name
