1580e28ef3bcdde9b95562d95ac12e944ec3927d
isaiasrider <isaiasdelima1@gmail.com>
2021-02-28 02:07:53 -0300

    fix typo in container_version

--- modules/services/webserver_cluster/script/user-data.sh
+++ modules/services/webserver_cluster/script/user-data.sh
@@ -5,4 +5,4 @@ container_version=${container_version}
 
 yum update -y
 yum install docker
-docker run -d -p80:3000 622021885326.dkr.ecr.us-east-2.amazonaws.com/devops:${GIT_COMMIT}
+docker run -d -p80:3000 622021885326.dkr.ecr.us-east-2.amazonaws.com/devops:${container_version}
