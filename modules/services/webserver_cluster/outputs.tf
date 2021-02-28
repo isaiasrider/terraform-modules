549b0fb4bfffa7877dbb0ce2a2625744f172caed
isaiasrider <isaiasdelima1@gmail.com>
2021-02-28 01:55:35 -0300

    remove unused output

--- modules/services/webserver_cluster/outputs.tf
+++ modules/services/webserver_cluster/outputs.tf
@@ -3,10 +3,6 @@ output "alb_dns_name" {
   description = "THE DOMAIN NAME OF THE LOAD BALANCER"
 }
 
-output "teste_remote" {
-  value = data.terraform_remote_state.db.outputs.db_url
-}
-
 output "sg_id" {
   value = aws_security_group.asg_security_group.id
 }
